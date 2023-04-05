ab6038242ed1b44753bc693dab3fe74cc50178e7
Frank <f.kinuthia01@gmail.com>
2023-04-05 12:20:11 +0300

    practice run

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-function scuberGreetingForFeet(heightInFeet){
-  // Write your code here!
-  if (heightInFeet <=400) {
-      return 'This one is on me!'
-  } 
-  else if (heightInFeet >=2000 && heightInFeet <2500){
-      return `I will gladly take your thirty bucks.`
-  }
+// function scuberGreetingForFeet(heightInFeet){
+//   // Write your code here!
+//   if (heightInFeet <=400) {
+//       return 'This one is on me!'
+//   } 
+//   else if (heightInFeet >=2000 && heightInFeet <2500){
+//       return `I will gladly take your thirty bucks.`
+//   }
     
-  else if (heightInFeet>2500) {
-        return `No can do.`
-    }
+//   else if (heightInFeet>2500) {
+//         return `No can do.`
+//     }
    
-} 
+// } 
+
+// switch statement for the above code
+function scuberGreetingForFeet(heightInFeet){
+  switch (true){
+      case heightInFeet <= 400:
+          return 'This one is on me!';
+      case heightInFeet > 2000 && heightInFeet < 2500:
+          return 'I will gladly take your thirty bucks.';
+      case heightInFeet > 2500:
+          return 'No can do.';
+      default:
+          return 'Invalid input';
+  }
+}
     
-  function ternaryCheckCity(city){
+function ternaryCheckCity(city){
   // Write your code here!
-    return city === "NYC"? `Ok, sounds good.`: `No go.`
-    }
+  return city === "NYC"? `Ok, sounds good.`: `No go.`
+}
 
 function switchOnCharmFromTip(tip){
   // Write your code here!
